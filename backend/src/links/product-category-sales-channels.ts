212bf9a1d215250995a4b2a93cbf45adb7e58c8a
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2025-05-21 07:13:20 -0300

    remove comments

--- backend/src/links/product-category-sales-channels.ts
+++ backend/src/links/product-category-sales-channels.ts
@@ -2,10 +2,7 @@ import { defineLink } from "@medusajs/framework/utils";
 import ProductModule from "@medusajs/medusa/product";
 import SalesChannelModule from "@medusajs/medusa/sales-channel";
 
-export default defineLink(
-  ProductModule.linkable.productCategory.id, // Link to the ProductCategory entity
-  {
-    likable: SalesChannelModule.linkable.salesChannel.id, // Link to the SalesChannel entity
-    isList: true, // A product category can be associated with multiple sales channels
-  }
-);
+export default defineLink(ProductModule.linkable.productCategory.id, {
+  likable: SalesChannelModule.linkable.salesChannel.id,
+  isList: true,
+});
