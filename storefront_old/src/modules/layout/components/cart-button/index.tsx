057a31fc4db2b2b275b2165a05fff36b6526bc02
Riqwan Thamir <rmthamir@gmail.com>
2025-09-02 12:04:24 +0200

    chore: remove old storefront code

--- storefront_old/src/modules/layout/components/cart-button/index.tsx
+++
@@ -1,8 +0,0 @@
-import { retrieveCart } from "@lib/data/cart"
-import CartDropdown from "@modules/layout/components/cart-dropdown"
-
-export default async function CartButton() {
-  const cart = await retrieveCart().catch(() => null)
-
-  return <CartDropdown cart={cart} />
-}
