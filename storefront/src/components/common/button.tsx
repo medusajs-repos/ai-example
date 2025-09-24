3e3355e4fce17824d897d1d5c2926c327dbd127e
Shahed Nasser <shahednasser@gmail.com>
2025-09-24 15:02:28 +0300

    updated button colors

--- storefront/src/components/common/button.tsx
+++ storefront/src/components/common/button.tsx
@@ -7,9 +7,9 @@ export const Button = (props: ButtonProps) => {
     <UiButton 
       {...props}
       className={clx({
-        "bg-button-primary-bg text-button-primary-text hover:bg-button-primary-hover active:bg-button-primary-pressed": props.variant === "primary",
-        "bg-button-secondary-bg text-button-secondary-text hover:bg-button-secondary-hover active:bg-button-secondary-pressed": props.variant === "secondary",
-        "bg-button-error-bg text-button-error-text hover:bg-button-error-hover active:bg-button-error-pressed": props.variant === "danger",
+        "bg-button-primary-bg text-button-primary-text hover:bg-button-primary-hover active:bg-button-primary-pressed shadow-button-primary active:shadow-button-primary-pressed": props.variant === "primary",
+        "bg-button-secondary-bg text-button-secondary-text hover:bg-button-secondary-hover active:bg-button-secondary-pressed shadow-button-secondary active:shadow-button-secondary-pressed": props.variant === "secondary",
+        "bg-button-error-bg text-button-error-text hover:bg-button-error-hover active:bg-button-error-pressed shadow-button-error active:shadow-button-error-pressed": props.variant === "danger",
       })}
     />
   )
