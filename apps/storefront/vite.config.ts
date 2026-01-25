3cd447eea6fcec10a83988f36ee6ed91b92f8a48
Riqwan Thamir <rmthamir@gmail.com>
2026-01-26 00:24:11 +0100

    chore: upgrade tanstack

--- apps/storefront/vite.config.ts
+++ apps/storefront/vite.config.ts
@@ -1,7 +1,7 @@
 import medusaAiTags from "@medusajs-ai/tags";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import react from "@vitejs/plugin-react";
+import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import Terminal from "vite-plugin-terminal";
 import viteTsConfigPaths from "vite-tsconfig-paths";
@@ -24,11 +24,8 @@ export default defineConfig(({ mode }) => {
           ]
         : []),
 
-      tanstackStart({
-        target: "netlify",
-        customViteReactPlugin: true,
-      }),
-      react(),
+      tanstackStart(),
+      viteReact(),
     ],
 
     ssr: {
